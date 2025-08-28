2812e20c86f4ecb8f6274f788a7dfb5612e82e06
jygaulier <gaulier@alchemy.fr>
2025-08-28 19:39:42 +0200

    WIP

--- README.md
+++ README.md
@@ -1,24 +1,62 @@
 # documentation builder
 
-## serve static documentation by integrated web server
-`dc up -d`
+Build the documentation from a phrasea release.
+- pull the `phrasea-doc.zip`.
+- build a static documentation with Docusaurus.
+- push the result to the github documentation repository (or serve it locally).
 
-## serve from the container
+## env-vars
+to define in `.env.local`
 
-http://localhost:3078/
+- `DOC_GITHUB_TOKEN`="github_pat_xxxx"
+
+    token with write (push) access to the documentation repository
+    
+    If not set, the builder will not push the result, but can serve it from the integrated web-server
 
 
-## dev mode
-`dc run --rm -p 3078:3000 documentation "pnpm run start"`
+- `PHRASEA_TAG`="x.y.z"
 
-or
+    phrasea tag to generate documentation for (e.g. "0.2.8").
 
-dc run --rm -p 3078:3000 documentation bash
+    If not set, takes the highest tag
 
-then in the container
+    The builder will push the documentation to a directory x.y (e.g. "0.2"), ignoring the 'z' patch number.
+
+## mode "build and push"
+
+**define** the `DOC_GITHUB_TOKEN` env-var into `.env.local`
+
+`dc up` will build the documentation in a clone of the documentation repository, then commit/push it back.
+
+The cloned repository is deleted afert push, so it is not possible to serve the documentation locally.
+
+## mode "build and serve"
+
+**comment** the `DOC_GITHUB_TOKEN` into `.env.local`
+
+`dc up` will build the documentation into a "build" directory
+and serve it on http://localhost:3078/
+
+
+## mode "developper"
+
+`dc run --rm -p 3078:3000 documentation bash`
+
+### app "builder"
+
+`cd` to the builder (php) app in `/srv/app`
+
+run `php application.php export --help`
+
+### app "docusaurus"
+
+`cd` to the the docusaurus / phrasea site in `/srv/docusaurus/phrasea`, run
 
 ```shell
 pnpm run start  # dev mode
 # or
 pnpm run serve
 ```
+
+
--- importer/Command/import.php
+++ importer/Command/import.php
@@ -2,30 +2,63 @@
 
 namespace App\Command;
 
+use PHLAK\SemVer;
 use SplFileInfo;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\IOException;
-use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(name: 'import')]
 class import extends Command
 {
-    private const GITHUB_API_URL = 'https://api.github.com';
+    private const string GITHUB_API_URL = 'https://api.github.com';
+    private const string DOCUSAURUS_PROJECT_DIR = __DIR__ . '/../../docusaurus/phrasea';
+    private const string WORKSPACE_DIR = __DIR__ . '/../workspace';
+    private const string CLONE_DIRNAME = 'phrasea-documentation';
+    private const string CLONE_DIR = self::WORKSPACE_DIR . '/' . self::CLONE_DIRNAME;
+    private const string DOCS_DIRNAME = 'docs';
+    private const string DOCS_DIR = self::CLONE_DIR . '/' . self::DOCS_DIRNAME;
+    private const string DOC_REPO = 'electrautopsy/pdoc.git';
+    private const string DOC_BRANCH = 'main';
+
     private InputInterface $input;
     private OutputInterface $output;
     private HttpClientInterface $httpClient;
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this
+            ->setDescription('Import documentation data from a phrasea release')
+            ->addArgument('tag', InputArgument::OPTIONAL)
+            ->addOption('list', 'l', InputOption::VALUE_NONE, 'list available tags and quit (no export)')
+        ;
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->input = $input;
         $this->output = $output;
+
+        $githubToken = getenv('DOC_GITHUB_TOKEN'); // Ensure the token is set in your environment
+        if (!$githubToken) {
+            $this->output->writeln('Warning: GitHub token not found in environment variables DOC_GITHUB_TOKEN');
+            $this->output->writeln('The builder will not be able to push the changes to the documentation repository.');
+        }
+
+        $filesystem = new Filesystem();
 
         $repository = 'alchemy-fr/sandbox-ci-documentation';
 
@@ -49,15 +82,88 @@ class import extends Command
                     }
                 }
             }
-            // todo: keep only the latest release of every major+minor version
-            // for now we keep the highest tag version
+
             uksort($releasesByTag, function ($a, $b) {
-                return version_compare($b, $a);
+                $a = new Semver\Version($a);
+                $b = new Semver\Version($b);
+                return $a->eq($b) ? 0 : ($a->gt($b) ? -1 : 1);
             });
-//            var_dump($releasesByTag);
-            foreach ($releasesByTag as $tag => $url) {
-                $this->generateForRelease($tag, $url);
-                break;  // only highest version for now
+            if($input->getOption('list')) {
+                $output->writeln("Available releases in $repository:");
+                foreach($releasesByTag as $tag => $url) {
+                    $output->writeln(" - $tag");
+                }
+                return Command::SUCCESS;
+            }
+
+            if(empty($releasesByTag)) {
+                throw new IOException("No release found in $repository");
+            }
+            $tag = array_key_first($releasesByTag);
+
+            if(null !== $input->getArgument('tag')) {
+                $tag = $input->getArgument('tag');
+                $this->output->writeln("Using tag from argument: " . $tag);
+            }
+            else if(null !== getenv('PHRASEA_TAG')) {
+                $tag = getenv('PHRASEA_TAG');
+                $this->output->writeln("Using tag from env-var PHRASEA_TAG: " . $tag);
+            }
+            if(!isset($releasesByTag[$tag])) {
+                throw new IOException("Tag $tag not found in releases");
+            }
+
+            // download the zip, unzip and prepare the 'docs' directory for docusaurus
+            $this->generateForRelease($tag, $releasesByTag[$tag]);
+
+            // create the api documentation from the json schema
+            $this->runCommand(
+                ['pnpm', 'run', 'gen-api-docs', 'databox'],
+                self::DOCUSAURUS_PROJECT_DIR
+            );
+
+            if($githubToken) {
+                // docusaurus will build directly in the workspace/repo directory, we first clone the doc repo
+                $filesystem->remove(self::WORKSPACE_DIR);
+                $filesystem->mkdir(self::WORKSPACE_DIR);
+
+                $this->runCommand(
+                    ['git', 'clone', "https://{{DOC_GITHUB_TOKEN}}@github.com/" . self::DOC_REPO, self::CLONE_DIRNAME],
+                    self::WORKSPACE_DIR
+                );
+
+                $semverTag = new Semver\Version($tag);
+                $version = $semverTag->major . '.' . $semverTag->minor;
+                $versionDir = sprintf('%s/%s', self::DOCS_DIR, $version);
+                $filesystem->mkdir($versionDir);
+
+                $this->runCommand(
+                    ['pnpm', 'build', '--out-dir', $versionDir],
+                    self::DOCUSAURUS_PROJECT_DIR,
+                    3600
+                );
+
+                $this->runCommand(['git', 'add', $versionDir], self::CLONE_DIR);
+                $commitMessage = sprintf('update %s on %s', $version, date('c'));
+                $this->runCommand(['git', 'commit', '-m', $commitMessage], self::CLONE_DIR);
+                $this->runCommand(['git', 'push', '-u', 'origin', self::DOC_BRANCH], self::CLONE_DIR);
+
+                $filesystem->remove(self::WORKSPACE_DIR);
+                $this->output->writeln(sprintf('Files committed and pushed successfully to %s.', $version));
+            }
+            else {
+                $this->output->writeln('No GitHub token provided, skipping commit and push.');
+                $this->runCommand(
+                    ['pnpm', 'build'],
+                    self::DOCUSAURUS_PROJECT_DIR,
+                    3600
+                );
+
+                $this->runCommand(
+                    ['pnpm', 'run', 'serve'],
+                    self::DOCUSAURUS_PROJECT_DIR,
+                    0
+                );
             }
 
             return Command::SUCCESS;
@@ -66,6 +172,36 @@ class import extends Command
             $output->writeln('Error: ' . $e->getMessage());
             return Command::FAILURE;
         }
+    }
+
+    private function runCommand(array $command, string $workingDir, int $timeout=60): void
+    {
+        $m = join(' ', array_map(
+            fn($m) => escapeshellcmd($m) === $m ? $m : escapeshellarg($m),
+            $command
+        ));
+        $this->output->writeln('<info>Running command:</info> ' . $m);
+
+        $command = array_map(function($c) {
+            return preg_replace_callback(
+                '|\{\{(\w+)}}|',
+                fn($m) => getenv($m[1]), $c);
+        }, $command);
+
+
+        $process = new Process($command, $workingDir);
+        $process->setTimeout($timeout);
+        $process->setIdleTimeout($timeout);
+
+        $process->run(function ($type, $buffer): void {
+            $this->output->write($buffer);
+        });
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $this->output->writeln($process->getOutput());
     }
 
     private function generateForRelease(string $tag, string $url): void
@@ -105,16 +241,19 @@ class import extends Command
         $this->compileFiles($unzipDir, $documentationDir);
     }
 
-    private function compileFiles(string $originDir, string $documentationDir): void
+    private function compileFiles(string $unzipDir, string $documentationDir): void
     {
         $filesystem = new Filesystem();
 
         $documentationDir = rtrim($documentationDir, '/');
-        $originDir = rtrim($originDir, '/');
-        $generatedDocDir = $originDir . '/_generatedDoc';
+        $unzipDir = rtrim($unzipDir, '/');
+        $generatedDocDir = $unzipDir . '/_generatedDoc';
         $generatedDocDirLen = strlen($generatedDocDir);
 
-        // ---- first dispatch _generatedDoc files to the matching directories
+        // ---- first dispatch _generatedDoc files to the same sub-directories in the zip directory
+        //      = move files one subdir up
+        $createdDirs = [];
+        $copiedFiles = [];
         try {
             $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($generatedDocDir, \FilesystemIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST);
             /** @var SplFileInfo $file */
@@ -123,26 +262,25 @@ class import extends Command
 
                 $this->output->writeln($relativePathname . ' => ' . $file->getFilename());
                 if ($file->isDir()) {
-                    $d = $originDir . $relativePathname;
+                    $d = $unzipDir . $relativePathname;
                     if (!$filesystem->exists($d)) {
                         $filesystem->mkdir($d);
                         $this->output->writeln("Created directory: $d");
+                        $createdDirs[] = $d;
                     }
                 }
                 elseif ($file->isFile()) {
-                    $d = $originDir . $relativePathname;
+                    $d = $unzipDir . $relativePathname;
                     if (!$filesystem->exists($d) || !file_exists($d)) {
                         $filesystem->copy($file->getPathname(), $d, true);
                         $this->output->writeln("Copied file: " . $file->getPathname() . " to " . $d);
+                        $copiedFiles[] = $d;
                     }
                     else {
                         $this->output->writeln("File already exists, skipping: " . $d);
                     }
                 }
             }
-
-            // remove the _generatedDoc directory
-            $filesystem->remove($originDir . '/_generatedDoc');
         }
         catch (\Exception $e) {
             // _generatedDoc directory can not exist
@@ -152,10 +290,10 @@ class import extends Command
 
         // ---- then dispatch the files in the documentation directory
         $translations = [];
-        $scan = function($rootdir, $subdir, $depth=0) use (&$scan, $filesystem, $documentationDir, &$translations) {
+        $scan = function($subdir, $depth=0) use (&$scan, $filesystem, $unzipDir, $documentationDir, &$translations) {
             $tab = str_repeat('  ', $depth);
-            $scandir =  $rootdir . $subdir;
-            $this->output->writeln(sprintf("%s Scanning %s", $tab, $scandir));
+            $scandir =  $unzipDir . $subdir;
+            $this->output->writeln(sprintf("%sScanning %s", $tab, $scandir));
             $iterator = new \DirectoryIterator($scandir);
             /** @var SplFileInfo $file */
             foreach ($iterator as $file) {
@@ -189,7 +327,7 @@ class import extends Command
                     else {
                         $targetDir = $documentationDir . '/i18n/' . $locale . '/docusaurus-plugin-content-docs/current/phrasea'. $subdir;
                     }
-                    $this->output->writeln(sprintf("%scopy %s to %s:%s", $tab, $file->getPathname(), $targetDir, $bn . $dotExtension));
+                    $this->output->writeln(sprintf("%s  copy %s to %s:%s", $tab, $file->getPathname(), $targetDir, $bn . $dotExtension));
                     $filesystem->mkdir($targetDir, 0777);
                     $filesystem->copy($file->getPathname(), $targetDir . '/' . $bn . $dotExtension, true);
 
@@ -206,13 +344,14 @@ class import extends Command
                         }
                     }
 
-                    $scan($rootdir, $subdir . '/' . $file->getFilename(), $depth + 1);
+                    $scan($subdir . '/' . $file->getFilename(), $depth + 1);
                 }
             }
-            $this->output->writeln(sprintf("%s Scanned %s:", $tab, $subdir));
         };
 
-        $scan($originDir, '') ;
+        $scan('');
+
+        $filesystem->remove($unzipDir);
 
         // dump translations to json files
         foreach ($translations as $locale => $translation) {
