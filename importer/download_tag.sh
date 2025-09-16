e02bf925491e6195f90b499053192ac99176f026
jygaulier <gaulier@alchemy.fr>
2025-09-16 20:09:05 +0200

    wip use script

--- importer/download_tag.sh
+++ importer/download_tag.sh
@@ -3,25 +3,25 @@ echo PHRASEA_GITHUB=$PHRASEA_GITHUB
 echo PHRASEA_IMAGES=$PHRASEA_IMAGES
 echo TAG=$1
 
-#mkdir -p ./downloads/$1
-#
-## copy the base doc from the GitHub repository
-## nb: sparse checkout is experimental and also clone some unwnanted files from / (like /README.md) ?
-##     so we clone in a tmp folder and then move the doc/ folder
-#
-#git clone --depth 1 --filter=blob:none --sparse https://github.com/$PHRASEA_GITHUB.git ./downloads/tmpclone/
-#cd ./downloads/tmpclone
-#git sparse-checkout set doc
-#git checkout $1
-#cd ../../
-#mv ./downloads/tmpclone/doc ./downloads/$1/
-#rm -rf ./downloads/tmpclone
-#
-#
-## copy the databox-api-php doc from the docker image
-#
-#mkdir -p ./downloads/$1/databox-api-php
-#docker pull $PHRASEA_IMAGES:$1
-#IMAGE_ID=$(docker create $PHRASEA_IMAGES:$1)
-#docker cp $IMAGE_ID:/srv/app/databox/api/doc/ ./downloads/$1/databox-api-php/ || echo "No doc/ folder found in image"
-#docker rm -v $IMAGE_ID
+mkdir -p ./downloads/$1
+
+# copy the base doc from the GitHub repository
+# nb: sparse checkout is experimental and also clone some unwnanted files from / (like /README.md) ?
+#     so we clone in a tmp folder and then move the doc/ folder
+
+git clone --depth 1 --filter=blob:none --sparse https://github.com/$PHRASEA_GITHUB.git ./downloads/tmpclone/
+cd ./downloads/tmpclone
+git sparse-checkout set doc
+git checkout $1
+cd ../../
+mv ./downloads/tmpclone/doc ./downloads/$1/
+rm -rf ./downloads/tmpclone
+
+
+# copy the databox-api-php doc from the docker image
+
+mkdir -p ./downloads/$1/databox-api-php
+docker pull $PHRASEA_IMAGES:$1
+IMAGE_ID=$(docker create $PHRASEA_IMAGES:$1)
+docker cp $IMAGE_ID:/srv/app/databox/api/doc/ ./downloads/$1/databox-api-php/ || echo "No doc/ folder found in image"
+docker rm -v $IMAGE_ID
