e84e73552bac00b32489ad1a46cd316a5d3178cd
jygaulier <gaulier@alchemy.fr>
2025-09-18 17:34:53 +0200

    wip

--- importer/download_tag.sh
+++ importer/download_tag.sh
@@ -13,9 +13,14 @@ git clone --depth 1 --filter=blob:none --sparse https://github.com/$PHRASEA_GITH
 cd ./importer/downloads/tmpclone
 git sparse-checkout set doc
 git checkout $1
+echo "================ tmpclone ================ "
+tree .
+
 cd ../../../
 mv ./importer/downloads/tmpclone/doc ./importer/downloads/$1/
 rm -rf ./importer/downloads/tmpclone
+echo "================ downloads $1 after gh clone ================ "
+tree ./importer/downloads/$1
 
 
 # copy the databox-api-php doc from the docker image
@@ -25,3 +30,6 @@ docker pull $PHRASEA_IMAGES:$1
 IMAGE_ID=$(docker create $PHRASEA_IMAGES:$1)
 docker cp $IMAGE_ID:/srv/app/databox/api/doc/ ./importer/downloads/$1/databox-api-php/ || echo "No doc/ folder found in image"
 docker rm -v $IMAGE_ID
+
+echo "================ downloads $1 after image pull ================ "
+tree ./importer/downloads/$1
