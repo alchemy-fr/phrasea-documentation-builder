79cad621067ecfb122d1de243bbf9c0200d01669
jygaulier <gaulier@alchemy.fr>
2025-09-18 17:48:42 +0200

    wip

--- importer/download_tag.sh
+++ importer/download_tag.sh
@@ -3,13 +3,12 @@ echo PHRASEA_GITHUB=$PHRASEA_GITHUB
 echo PHRASEA_IMAGES=$PHRASEA_IMAGES
 echo TAG=$1
 
-mkdir -p ./downloads/$1
+mkdir -p ./importer/downloads/$1
 
 # copy the base doc from the GitHub repository
 # nb: sparse checkout is experimental and also clone some unwnanted files from / (like /README.md) ?
 #     so we clone in a tmp folder and then move the doc/ folder
 
-# mkdir -p ./importer/downloads/$1/doc
 git clone --depth 1 --filter=blob:none --sparse https://github.com/$PHRASEA_GITHUB.git ./importer/downloads/tmpclone/
 cd ./importer/downloads/tmpclone
 git sparse-checkout set doc
