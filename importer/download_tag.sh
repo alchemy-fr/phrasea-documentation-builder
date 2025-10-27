c3d8ff5510c5b4e1b673aa80ceeedb1aecf2981c
jygaulier <gaulier@alchemy.fr>
2025-10-27 11:18:27 +0100

    debug

--- importer/download_tag.sh
+++ importer/download_tag.sh
@@ -5,8 +5,6 @@ echo TAG: $1
 mkdir -p ./downloads/$TAG/src
 
 # copy the base doc from the GitHub repository
-# nb: sparse checkout is experimental
-#     we clone in a tmp folder and then move the doc/ folder
 
 git clone --filter=blob:none --no-checkout https://github.com/alchemy-fr/phrasea.git ./downloads/tmpclone/
 cd ./downloads/tmpclone
@@ -14,6 +12,7 @@ git fetch origin
 git checkout $TAG
 cd ../../
 cp -r  ./downloads/tmpclone/doc ./downloads/$TAG/src/
+
 
 cat ./downloads/tmpclone/doc/include.list | while read -r p; do
   if [[ -d "./downloads/tmpclone/$p" ]]; then
@@ -25,6 +24,9 @@ cat ./downloads/tmpclone/doc/include.list | while read -r p; do
 done
 rm -rf ./downloads/tmpclone
 
+echo --------- after git clone
+tree ./downloads/
+
 # copy the databox-api-php doc from the docker image
 
 mkdir -p ./downloads/$TAG/_generated/databox
@@ -32,3 +34,6 @@ docker pull public.ecr.aws/alchemyfr/ps-databox-api-php:$TAG
 IMAGE_ID=$(docker create public.ecr.aws/alchemyfr/ps-databox-api-php:$TAG)
 docker cp $IMAGE_ID:/srv/app/doc ./downloads/$TAG/_generated/databox || echo "No /srv/app/doc folder found in image"
 docker rm -v $IMAGE_ID
+
+echo --------- after docker pull
+tree ./downloads/
