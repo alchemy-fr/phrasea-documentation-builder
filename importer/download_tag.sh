bd96e44e96b9f00a21b84f726417502b808cb661
jygaulier <gaulier@alchemy.fr>
2025-10-27 11:40:53 +0100

    debug

--- importer/download_tag.sh
+++ importer/download_tag.sh
@@ -2,38 +2,42 @@
 TAG=$1
 echo TAG: $1
 
-mkdir -p ./downloads/$TAG/src
+cd ./importer/downloads
+
+mkdir -p ./$TAG/src
 
 # copy the base doc from the GitHub repository
 
-git clone --filter=blob:none --no-checkout https://github.com/alchemy-fr/phrasea.git ./downloads/tmpclone/
-cd ./downloads/tmpclone
+git clone --filter=blob:none --no-checkout https://github.com/alchemy-fr/phrasea.git ./tmpclone/
+cd ./tmpclone
 git fetch origin
 git checkout $TAG
-cd ../../
-cp -r  ./downloads/tmpclone/doc ./downloads/$TAG/src/
+cd ..
+cp -r  ./tmpclone/doc ./$TAG/src/
 
 
-cat ./downloads/tmpclone/doc/include.list | while read -r p; do
-  if [[ -d "./downloads/tmpclone/$p" ]]; then
-    mkdir -p "./downloads/$TAG/src/doc/$p"
-    cp -r "./downloads/tmpclone/$p" "./downloads/$TAG/src/doc/$p/.."
-  elif [[ -f "./downloads/tmpclone/$p" ]]; then
-    install -D "./downloads/tmpclone/$p" "./downloads/$TAG/src/doc/$p"
+cat ./tmpclone/doc/include.list | while read -r p; do
+  if [[ -d "./tmpclone/$p" ]]; then
+    mkdir -p "./$TAG/src/doc/$p"
+    cp -r "./tmpclone/$p" "./$TAG/src/doc/$p/.."
+  elif [[ -f "./tmpclone/$p" ]]; then
+    install -D "./tmpclone/$p" "./$TAG/src/doc/$p"
   fi
 done
-rm -rf ./downloads/tmpclone
+rm -rf ./tmpclone
 
 echo --------- after git clone
-tree ./downloads/
+tree .
 
 # copy the databox-api-php doc from the docker image
 
-mkdir -p ./downloads/$TAG/_generated/databox
+mkdir -p ./$TAG/_generated/databox
 docker pull public.ecr.aws/alchemyfr/ps-databox-api-php:$TAG
 IMAGE_ID=$(docker create public.ecr.aws/alchemyfr/ps-databox-api-php:$TAG)
-docker cp $IMAGE_ID:/srv/app/doc ./downloads/$TAG/_generated/databox || echo "No /srv/app/doc folder found in image"
+docker cp $IMAGE_ID:/srv/app/doc ./$TAG/_generated/databox || echo "No /srv/app/doc folder found in image"
 docker rm -v $IMAGE_ID
 
 echo --------- after docker pull
-tree ./downloads/
+tree .
+
+cd ../..
